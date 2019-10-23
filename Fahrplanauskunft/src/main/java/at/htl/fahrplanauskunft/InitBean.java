95696845cf7d60796d7f7912478f69381c7c0d1f
Simon <simon.ittensammer@gmx.at>
2019-10-23 22:57:22 +0200

    add some sample Lines

--- Fahrplanauskunft/src/main/java/at/htl/fahrplanauskunft/InitBean.java
+++ Fahrplanauskunft/src/main/java/at/htl/fahrplanauskunft/InitBean.java
@@ -1,5 +1,26 @@
 package at.htl.fahrplanauskunft;
 
+import at.htl.fahrplanauskunft.entity.Linie;
+import at.htl.fahrplanauskunft.entity.Transportmittel;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@ApplicationScoped
+@Transactional
 public class InitBean {
-    
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void init(@Observes @Initialized(ApplicationScoped.class) Object init) {
+        System.out.println("*** It works! ***");
+
+        em.persist(new Linie("Tragwein", "Linz Hauptbahnhof", Transportmittel.BUS));
+        em.persist(new Linie("Linz Hauptbahnhof", "Meixnerkreuzung", Transportmittel.STRASSENBAHN));
+    }
 }
