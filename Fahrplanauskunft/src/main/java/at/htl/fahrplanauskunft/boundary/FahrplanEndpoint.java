d5bdcf752ceb833dbb39483ec9aa57560d0e6faf
Simon <simon.ittensammer@gmx.at>
2019-10-23 22:30:34 +0200

    add Method init to FahrplanEndpoint.java

--- Fahrplanauskunft/src/main/java/at/htl/fahrplanauskunft/boundary/FahrplanEndpoint.java
+++ Fahrplanauskunft/src/main/java/at/htl/fahrplanauskunft/boundary/FahrplanEndpoint.java
@@ -1,5 +1,26 @@
 package at.htl.fahrplanauskunft.boundary;
 
+import at.htl.fahrplanauskunft.entity.Linie;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Stateless
+@Path("/")
 public class FahrplanEndpoint {
-    
+    @PersistenceContext
+    EntityManager em;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("FahrplanEndpoint created ***");
+    }
+
 }
