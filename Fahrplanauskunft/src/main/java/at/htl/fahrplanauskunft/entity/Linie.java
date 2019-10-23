bb047dedd20c3aada6a5e40b434c04a7d3d2b1f0
Simon <simon.ittensammer@gmx.at>
2019-10-23 22:36:54 +0200

    add Attributes and Constructors to Class Linie

--- Fahrplanauskunft/src/main/java/at/htl/fahrplanauskunft/entity/Linie.java
+++ Fahrplanauskunft/src/main/java/at/htl/fahrplanauskunft/entity/Linie.java
@@ -1,4 +1,30 @@
 package at.htl.fahrplanauskunft.entity;
 
-public class Linie {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class Linie implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    String abfahrtsOrt;
+    String ankunftsOrt;
+    Transportmittel transportmittel;
+    List<Haltestelle> haltestellen;
+
+    public Linie(String abfahrtsOrt, String ankunftsOrt, Transportmittel transportmittel, List<Haltestelle> haltestellen) {
+        this.abfahrtsOrt = abfahrtsOrt;
+        this.ankunftsOrt = ankunftsOrt;
+        this.transportmittel = transportmittel;
+        this.haltestellen = haltestellen;
+    }
+
+    public Linie() {
+
+    }
 }
